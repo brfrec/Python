cf03a06fc38f4e05192ac33cf66a39017e68875d
brfrec <charloutre34@gmail.com>
2025-05-18 16:37:13 +0200

    Amélioration

--- euromillion.py
+++ euromillion.py
@@ -1,5 +1,9 @@
-import random
+# -*- coding: utf-8 -*- 
 
+import random
+import os
+os.system("clear")
+print("Voici les prochains numéros gagnants de l'euromillion !")
 
 liste_numero = []
 liste_numero_etoile = []
@@ -8,8 +12,8 @@ for i in range(1, 50):
 for i in range(1, 11):
     liste_numero_etoile.append(i) 
 numero1 = random.randint(1,49)
-print(liste_numero)
-print("\033[0;32m Voici le premier numéro : \033[0m" + str(numero1))
+
+print("\033[0;32mVoici le premier numéro : \033[0m" + str(numero1))
 numero2 = random.randint(1,49)
 while numero2 == numero1:
     numero2 = random.randint(1,49) 
@@ -28,7 +32,7 @@ while numero5 == numero1 or numero5 == numero2 or numero5 == numero3 or numero5 
 print("\033[0;32mVoici le cinquième numéro : \033[0m" + str(numero5))
 
 etoile1 = random.randint(1,10)
-print("\033[0;35mVoici la première étoile : \033[0m" + str(etoile1))
+print("\n\033[0;35mVoici la première étoile : \033[0m" + str(etoile1))
 etoile2 = random.randint(1,10)
 while etoile2 == etoile1:
     etoile2 = random.randint(1,10)
