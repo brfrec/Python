d324a8c9dec0d48cf54416dcd250ac8bb464561f
brfrec <charloutre34@gmail.com>
2025-05-18 16:24:00 +0200

    amélioration

--- euromillion.py
+++ euromillion.py
@@ -1,8 +1,14 @@
 import random
 
-liste_numero = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]
-liste_numero_etoile = [1,2,3,4,5,6,7,8,9,10]
+
+liste_numero = []
+liste_numero_etoile = []
+for i in range(1, 50):
+    liste_numero.append(i)
+for i in range(1, 11):
+    liste_numero_etoile.append(i) 
 numero1 = random.randint(1,49)
+print(liste_numero)
 print("\033[0;32m Voici le premier numéro : \033[0m" + str(numero1))
 numero2 = random.randint(1,49)
 while numero2 == numero1:
