c950e6202fe0c135369ea2a562270dfb3368dd18
brfrec <charloutre34@gmail.com>
2025-05-19 12:40:28 +0200

    ajout de nouvelle fonction

--- Test.py
+++ Test.py
@@ -1,11 +1,15 @@
 import random
+import os
+os.system("clear")
 
-
-listeUn = [" Chevreuil ", " Lapin ", " Souris ", " Zèbre ", " Renard ", " Chien ", " Coq "]
-listeDeux = [" caline ", "baise", "suce", "encule", "doigte", "caresse", "enfile", "gicle sur"]
+listeUn = [" La hyène "," Le chevreuil ", " Le lapin ", " La souris ", " Le zèbre ", " Le renard ", " Le chien ", " Le coq "]
+listeDeux = ["pisse sur","chie sur","se fait prendre le fifi par","baise", "suce", "encule", "doigte", "fais mousser", "enfile", "gicle sur"]
 aleatoire1 = random.randint(0,len(listeUn)-1)
 aleatoire2 = random.randint(0,len(listeDeux)-1)
 aleatoire3 = random.randint(0,len(listeUn)-1)
+if aleatoire1 == aleatoire3:
+    aleatoire3 = random.randint(0,len(listeUn)-1)
+
 print(listeUn[aleatoire1] + listeDeux[aleatoire2] + listeUn[aleatoire3])
 
 
