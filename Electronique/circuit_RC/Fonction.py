32de4912a33219ddf0e7f1460d596c3aec101661
brfrec <charloutre34@gmail.com>
2025-05-28 21:52:48 +0200

    Opérationnel mais a améliorer

--- Electronique/circuit_RC/Fonction.py
+++ Electronique/circuit_RC/Fonction.py
@@ -1,13 +1,12 @@
+from FonctionCalcul import *
+
+
+
 
 def Explication():
     print("Bienvenue dans le programme de calcul de circuits RC.")
     print("Ce programme vous permettra de calculer la valeur d'une résistance, d'un condensateur ou le temps de charge/décharge (Tau) d'un circuit RC.")
     print("Veuillez suivre les instructions pour entrer les valeurs nécessaires.")
     print("\n")  # Ajout d'une ligne vide pour une meilleure lisibilité
-
-
-
-
-
 
 
--- Electronique/circuit_RC/FonctionCalcul.py
+++ Electronique/circuit_RC/FonctionCalcul.py
@@ -13,52 +13,63 @@ def calculResistance():
         if R < 1:
             R = R * 1000
             print("La valeur de la résistance est :",R," mHoms")
+            
         elif R > 1 and R < 1000:
             print("La valeur de la résistance est :", R, " Ohms")
+           
         elif R > 1000 and R < 1000000:
             R = R / 1000
-            print("La valeur de la résistance est :", R, " KOhms")  
+            print("La valeur de la résistance est :", R, " KOhms")
+             
         elif R > 1000000 and R < 1000000000:
             R = R / 1000000
             print("La valeur de la résistance est :", R, " MOhms")      
-        
+            
+
     except ValueError:
         print("Erreur : Veuillez entrer des valeurs numériques valides.")   
-    
+ 
 def calculCondensateur():
     print("Calcul du condensateur")
     
     try:
         R = float(input("Entrez la valeur de la résistance : "))
         Tau5 = float(input("Entrez le temps de charge/décharge 5 tau : "))
-        Tau = Tau5 / 5  # Calcul de Tau à partir de 5 Tau
-        print("Tau = ", Tau, "s")
-    except: print("Erreur : Veuillez entrer une valeur numérique valide pour la résistance.")   
+       
+    except: print("Erreur : Veuillez entrer une valeur numérique valide pour la résistance.")
+    Tau = Tau5 / 5  # Calcul de Tau à partir de 5 Tau
+    print("Tau = ", Tau, "s")   
     C = Tau / R
     if C < 1e-6:
         C = C * 1e9
         print("La valeur du condensateur est :",ceil(C) , "nF")
+       
     elif C >= 1e-6 and C < 1:
         C = C * 1e6
         print("La valeur du condensateur est :",ceil(C) , "µF")
         
+ 
+
 def calculTau():
     print("Calcul du temps de charge/décharge (Tau)")
 
     try:
         R = float(input("Entrez la valeur de la résistance : "))
         C = float(input("Entrez la valeur du condensateur en µF : "))
-        Tau = R * (C* 1e-6)  # Conversion de µF à F
-        if Tau < 1e-3:
-            Tau = ceil(Tau * 1e6)
-            print("Tau = ",Tau,"µs\n3 Tau = ", Tau * 3, "µs\n5 Tau = ", Tau * 5, "µs")
-        elif Tau >= 1e-3 and Tau < 1:
-            Tau = Tau * 1e3
-            print("Tau = ",Tau,"ms\n3 Tau = ", Tau * 3, "ms\n5 Tau = ", Tau * 5, "ms")
-        else:
-            print("Tau = ",Tau,"s\n3 Tau = ", ceil(Tau * 3), "s\n5 Tau = ", ceil(Tau * 5), "s")
-                
     except ValueError:
         print("Erreur : Veuillez entrer des valeurs numériques valides.")
+        
+    Tau = R * (C* 1e-6)  # Conversion de µF à F
+    if Tau < 1e-3:
+        Tau = ceil(Tau * 1e6)
+        print("Tau = ",ceil(Tau),"µs\n3 Tau = ", ceil(Tau * 3), "µs\n5 Tau = ", ceil(Tau * 5), "µs")
+    elif Tau >= 1e-3 and Tau < 1:
+        Tau = Tau * 1e3
+        print("Tau = ",ceil(Tau),"ms\n3 Tau = ", ceil(Tau * 3), "ms\n5 Tau = ", ceil(Tau * 5), "ms")
+    else:
+        print("Tau = ",ceil(Tau),"s\n3 Tau = ", ceil(Tau * 3), "s\n5 Tau = ", ceil(Tau * 5), "s")
+                
+    
+
 
 
