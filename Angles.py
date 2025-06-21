d021a983a59fc4e6423ddcf5272471b669974c80
brfrec <charloutre34@gmail.com>
2025-06-21 21:31:38 +0200

    Retour

--- Angles.py
+++ Angles.py
@@ -3,9 +3,9 @@ from math import *
 
 def calculAngles (AB,BC,AC):
     angleRadianA = acos(AB/AC)
-    angleDegreA = round (degrees(angleRadianA), 2)
+    angleDegreA = degrees(angleRadianA)
     angleRadianB = acos(BC/AC)
-    angleDegreB = round(degrees(angleRadianB), 2)
+    angleDegreB = degrees(angleRadianB)
     print("L'angle A vaut : ", angleDegreA )
     print("L'angle B vaut : ", angleDegreB )
     
